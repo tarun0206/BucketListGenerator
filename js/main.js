bb5c09db9afffa4030ea802558fd0c4f7db917d6
tarun0206 <tarun.singh.0206@gmail.com>
2022-08-30 01:19:24 +0530

    added css and javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,22 @@
 
+document.getElementById('submit').addEventListener('click', submit)
+document.getElementById('secondButton').addEventListener('click', submit)
+
+var myHeaders = new Headers();
+myHeaders.append("X-Api-key", "BAqSh4c2MJU/zMz0/AuESg==3Qodr0PhBtQFctjp");
+
+var requestOptions = {
+  method: 'GET',
+  headers: myHeaders,
+  redirect: 'follow'
+};
+
+function submit(){
+    document.getElementById('firstDisplay').style.display = 'none'
+    fetch("https://api.api-ninjas.com/v1/bucketlist", requestOptions)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('placeIdeas').innerHTML = data.item
+        document.getElementById('section').style.display = 'flex'
+    })
+}
